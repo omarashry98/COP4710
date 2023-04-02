9d42f1db81ed28487aad6414b82c0621fc5fa358
Reynaldo-Martinez2021 <89822899+Reynaldo-Martinez2021@users.noreply.github.com>
2023-04-02 15:04:59 -0400

    Added CreateEvent php file

---
+++ LAMPAPI/FetchAllEvents.php
@@ -0,0 +1,58 @@
+<?php
+    $inData = getRequestInfo();
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4710");
+    if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	} else {
+
+        $stmt = $conn->prepare("SELECT id, name, category, description, date, location, latitude, longitude, contact_phone, contact_email, event_type, rso_id, admin_id, start_time, end_time, university_id FROM Events");
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $universities = array();
+
+        while ($row = $result->fetch_assoc())
+        {
+            $universities[] = $row;
+        }
+
+        if (count($universities) > 0)
+        {
+            returnWithInfo($universities);
+        } else {
+            returnWithError("No universities found.");
+            http_response_code(404); // Not Found
+        }
+
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"error":"' . $err . '"}';
+
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo( $universities )
+    {
+        $retValue = json_encode($universities);
+        sendResultInfoAsJson( $retValue );
+    }
+?>
